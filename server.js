3cc1508558e94e8abcab395c871cebacf7a2836d
Shubham Silyan <shubham7silyan@gmail.com>
2025-08-25 20:24:26 +0530

    Fix server to serve frontend from local build directory

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use('/api/lists', require('./routes/lists'));
 
 // Serve static files from React build
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
